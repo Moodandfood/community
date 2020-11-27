3f9b2508fe1ddf454df891e764ae66d87d30d1ca
minmin <minmin@163.comgit config --listgit config user.namegit config --global user.email minmin@163.com>
2020-11-27 16:53:00 +0800

    init repo

---
+++ src/main/java/com/mmm/community/cotroller/DemoController.java
@@ -0,0 +1,16 @@
+package com.mmm.community.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
